af45f60450bb0b8425cc2500eab503a512e450f0
seunope <seunope2004@yahoo.co.uk>
2021-11-11 17:20:47 +0100

    add auth to endpoint

--- src/routes/questions.route.ts
+++ src/routes/questions.route.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import QuestionsController from '@controllers/questions.controller';
 import { CreateQuestionDto } from '@dtos/questions.dto';
 import { Routes } from '@interfaces/routes.interface';
+import authMiddleware from '@middlewares/auth.middleware';
 import validationMiddleware from '@middlewares/validation.middleware';
 
 class QuestionsRoute implements Routes {
@@ -15,10 +16,15 @@ class QuestionsRoute implements Routes {
 
   private initializeRoutes() {
     this.router.get(`${this.path}`, this.questionsController.getQuestions);
-    this.router.get(`${this.path}/:id(\\d+)`, this.questionsController.getQuestionById);
-    this.router.post(`${this.path}`, validationMiddleware(CreateQuestionDto, 'body'), this.questionsController.createQuestion);
-    this.router.put(`${this.path}/:id(\\d+)`, validationMiddleware(CreateQuestionDto, 'body', true), this.questionsController.updateQuestion);
-    this.router.delete(`${this.path}/:id(\\d+)`, this.questionsController.deleteQuestion);
+    this.router.get(`${this.path}/:id(\\d+)`, authMiddleware, this.questionsController.getQuestionById);
+    this.router.post(`${this.path}`, authMiddleware, validationMiddleware(CreateQuestionDto, 'body'), this.questionsController.createQuestion);
+    this.router.put(
+      `${this.path}/:id(\\d+)`,
+      authMiddleware,
+      validationMiddleware(CreateQuestionDto, 'body', true),
+      this.questionsController.updateQuestion,
+    );
+    this.router.delete(`${this.path}/:id(\\d+)`, authMiddleware, this.questionsController.deleteQuestion);
   }
 }
 
